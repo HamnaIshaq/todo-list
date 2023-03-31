d8de524aa94b50953ba9abb25db66f13ea586a32
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-03-31 22:11:52 +0500

    Import and call modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import "./assets/styles/style.css";
 
 import { Initialize } from "./modules/Initialize";
+import { AddTodoUI } from "./modules/UI/AddTodoUI";
+import { DeleteTodo } from "./modules/UI/DeleteTodo";
+import { AddProject } from "./modules/UI/AddProject";
+
+let allProjects = [];
+allProjects.push(Initialize);
 
 const root = document.querySelector("#root");
+AddTodoUI(root, Initialize);
+DeleteTodo(root, Initialize);
+AddProject(root, allProjects);
