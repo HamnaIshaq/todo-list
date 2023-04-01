9babe39a9abb8448b94e449e48e680ca7d70224a
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-04-02 00:30:09 +0500

    Show selected project, remove add and delete todo logic from index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import "./assets/styles/style.css";
 
 import { Initialize } from "./modules/Initialize";
-import { AddTodoUI } from "./modules/UI/AddTodoUI";
-import { DeleteTodo } from "./modules/UI/DeleteTodo";
 import { AddProject } from "./modules/UI/AddProject";
+import { SelectProject } from "./modules/UI/SelectProject";
 
 let allProjects = [];
 allProjects.push(Initialize);
 
+let selectedProject = Initialize;
+
 const root = document.querySelector("#root");
-AddTodoUI(root, Initialize);
-DeleteTodo(root, Initialize);
+
 AddProject(root, allProjects);
+SelectProject(root, allProjects, selectedProject);
