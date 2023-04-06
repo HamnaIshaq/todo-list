2eda81462aef19f14a4d65a714af20aef1022763
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-04-07 00:53:46 +0500

    Call all modules in main file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import "./assets/styles/style.css";
 import { Initialize } from "./modules/Initialize";
 import { AddProject } from "./modules/UI/AddProject";
 import { SelectProject } from "./modules/UI/SelectProject";
+import { OpenCloseModal, CloseModalWithCrossBtn } from "./modules/UI/Modal";
+import { AddTodo } from "./modules/UI/AddTodo";
+import { DeleteTodo } from "./modules/UI/DeleteTodo";
+import { TodoDetails } from "./modules/UI/TodoDetails";
+import { TodoStatus } from "./modules/UI/TodoStatus";
 
 let allProjects = [];
 allProjects.push(Initialize);
@@ -11,5 +16,13 @@ let selectedProject = Initialize;
 
 const root = document.querySelector("#root");
 
+OpenCloseModal("addTodoModal", "addTodoModalBtn");
+CloseModalWithCrossBtn("close");
+//OpenCloseModal("updateTodoModal", "updateTodoModalBtn");
+
 AddProject(root, allProjects);
 SelectProject(root, allProjects, selectedProject);
+AddTodo(root, allProjects);
+DeleteTodo(root, allProjects);
+TodoDetails(allProjects);
+TodoStatus(root, allProjects);
