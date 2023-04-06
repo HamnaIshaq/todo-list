092cce3b5855351855d7708dac9c2a7917bdcc71
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-04-07 00:47:11 +0500

    Update delete todo logic

--- src/modules/UI/DeleteTodo.js
+++ src/modules/UI/DeleteTodo.js
@@ -1,16 +1,50 @@
-export const DeleteTodo = (root, selectedProject) => {
+import { RemoveModalListeners, OpenCloseModal } from "./Modal";
+
+/*
+  DELETE TODO LOGIC
+
+  ON CLICKING DELETE BTN
+  DELETE THE CLICKED TODO FROM SELECTED PROJECT
+  REMOVE PREVIOUS EVENT LISTENERS FOR ALL UPDATE TODO BTNS AND MODAL
+  ADD EVENT LISTENERS FOR ALL UPDATE TODO BTNS AND MODAL
+  DELETE TODO ON THE DOM
+*/
+
+export const DeleteTodo = (root, allProjects) => {
   const todoContainer = root.querySelector(".todo-container");
+  const emptyTodoMessage = root.querySelector(".empty-todos-container");
 
   todoContainer.addEventListener("click", (e) => onClickDeleteTodoBtn(e));
 
   function onClickDeleteTodoBtn(e) {
     if (e.target.classList.contains("delete-todo-btn")) {
-      const clickedTodoCard = e.target.parentElement.parentElement;
+      let clickedTodoCard;
+
+      e.target.tagName.toLowerCase() === "img"
+        ? (clickedTodoCard = e.target.parentElement.parentElement.parentElement)
+        : (clickedTodoCard = e.target.parentElement.parentElement);
+
       const projectId = clickedTodoCard.getAttribute("data-todo-id");
 
-      clickedTodoCard.remove();
+      clickedTodoCard.parentElement.remove();
+
+      if (todoContainer.children.length === 0) {
+        emptyTodoMessage.classList.remove("hidden");
+        emptyTodoMessage.classList.add("block");
+      }
+
+      let selectedProject;
+
+      allProjects.forEach((project) => {
+        if (project.active === true) {
+          selectedProject = project;
+        }
+      });
 
       selectedProject.deleteProjectTodo(projectId);
+
+      RemoveModalListeners("updateTodoModal", "updateTodoModalBtn");
+      OpenCloseModal("updateTodoModal", "updateTodoModalBtn");
     }
   }
 };
