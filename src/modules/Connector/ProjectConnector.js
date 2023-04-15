b7fc3b0989a92d15be27ba2e2ae2854cd7005e26
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-04-15 20:55:40 +0500

    Delete un-used folder and files

--- src/modules/Connector/ProjectConnector.js
+++
@@ -1,9 +0,0 @@
-//import Project from "../App Logic/Project"; // MODEL
-
-const ProjectConnector = () => {
-  //console.log(new Project("title"));
-
-  console.log("connector project");
-};
-
-export default ProjectConnector;
